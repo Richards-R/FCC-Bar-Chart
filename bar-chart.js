9cb5a093de79ccd9385ce4e693185f7ef0582be1
Reon-Richards <rr7366@gmail.com>
2023-06-25 22:23:03 +0900

    Updated all 14/14

--- bar-chart.js
+++ bar-chart.js
@@ -18,81 +18,73 @@ let svg = d3.select('svg')
 
 let generateScales = () => {
   yScale = d3.scaleLinear()
-  .domain([0, d3.max(graphArr, (d) => {
-   return d[1]
-  })])
-  .range([0, h - (2*padding)]);
+    .domain([0, d3.max(graphArr, (d) => {
+    return d[1]
+    })])
+    .range([0, h - (2*padding)]);
   
   xScale = d3.scaleLinear()
-  .domain([0, (graphArr.length -1)])
-  .range([padding, w - padding]);
-  
-
+    .domain([0, (graphArr.length -1)])
+    .range([padding, w - padding]);
   
   datesArr = graphArr.map((item) => {
-  return new Date(item[0])
-  })
-  
-  console.log(graphArr)
-  console.log(datesArr)
+    return new Date(item[0])
+    })
   
   xAxisScale = d3.scaleTime()
-  .domain([d3.min(datesArr), d3.max(datesArr)])
-  .range([padding, w - padding])
+    .domain([d3.min(datesArr), d3.max(datesArr)])
+    .range([padding, w - padding])
   
   yAxisScale = d3.scaleLinear()
-  .domain([0, d3.max(graphArr, (d) => {
-   return d[1]})])
-  .range([h-padding, padding])
-}
+    .domain([0, d3.max(graphArr, (d) => {
+    return d[1]})])
+    .range([h-padding, padding])
+    }
 
-  let drawCanvas = () => {
-    svg.attr("width", w)
-    svg.attr("height", h)
+let drawCanvas = () => {
+  svg.attr("width", w)
+  svg.attr("height", h)
   }
 
 let drawBars =()=>{
   svg.selectAll("rect")
-  .data(graphArr)
-  .enter()
-  .append("rect")
-  .attr("class", "bar")
-  .attr("width", (w - (2*padding)) / graphArr.length)
-  .attr("data-date", (item) => {
+    .data(graphArr)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("width", (w - (2*padding)) / graphArr.length)
+    .attr("data-date", (item) => {
     return item[0]
-  })
-  .attr("data-gdp", (item) => {
-    return item[1]
-  })
-  .attr("height", (item) => {
-  return yScale(item[1])
-  })
-  .attr("x", (item, i)=> {
-   return xScale(i);
     })
-  .attr("y", (item) => {
-  return (h - padding) - yScale(item[1])})
-
-  .attr("fill", (d, i) => {
-  if (i % 2 === 0){
-  return "violet"}
-  else {
-  return "pink"}
-  })
-
-  .on("mouseover", (event, item)=>{
-    drawToolTip.transition()
-    .style("visibility", "visible")
-
-    drawToolTip.text(item[0])
-
-    document.querySelector("#tooltip").setAttribute("data-date", item[0])
-  })
-
-  .on("mouseout", (event, item) => {
-    drawToolTip.transition()
-    .style("visibility", "hidden")
-  })
+    .attr("data-gdp", (item) => {
+    return item[1]
+    })
+    .attr("height", (item) => {
+    return yScale(item[1])
+    })
+    .attr("x", (item, i)=> {
+    return xScale(i);
+    })
+    .attr("y", (item) => {
+    return (h - padding) - yScale(item[1])
+    })
+    .attr("fill", (d, i) => {
+      if (i % 2 === 0){
+        return "violet"
+      }else{
+        return "pink"
+      }
+    })
+    .on("mouseover", (event, item)=>{
+      drawToolTip.transition()
+      .style("visibility", "visible")
+      drawToolTip.text(`Date: ${item[0]} Index: ${item[1]}`)
+      document.querySelector("#tooltip").setAttribute("data-date", item[0])
+    })
+    .on("mouseout", (event, item) => {
+      drawToolTip.transition()
+      .style("visibility", "hidden")
+    })
 }
 
 let generateAxes=()=>{
@@ -101,27 +93,24 @@ let generateAxes=()=>{
   let yAxis = d3.axisLeft(yAxisScale);
 
   svg.append("g")
-  .call(xAxis)
-  .attr("transform", "translate(0," + (h - padding) + ")")
-  .attr('id','x-axis')
-
+    .call(xAxis)
+    .attr("transform", "translate(0," + (h - padding) + ")")
+    .attr('id','x-axis')
 
   svg.append("g")
-  .call(yAxis)
-  .attr("transform", "translate (" + (padding) + ", 0)")
-  .attr('id','y-axis')
-  }
+    .call(yAxis)
+    .attr("transform", "translate (" + (padding) + ", 0)")
+    .attr('id','y-axis')
+}
 
 req.open("GET", url, true);
 req.onload = () => {
-json = JSON.parse(req.responseText)
-graphArr = json.data
-console.log(graphArr)
-drawCanvas()
-generateScales()
-drawBars()
-generateAxes()
-//drawToolTip()
+  json = JSON.parse(req.responseText)
+  graphArr = json.data
+  drawCanvas()
+  generateScales()
+  drawBars()
+  generateAxes()
 };
 req.send();
 
@@ -132,8 +121,5 @@ let drawToolTip = d3.select("body")
   .attr("y", "50%")
   .style("width", "auto")
   .style("height", "auto")
-  .attr("fill", "red")
   .style("visibility", "hidden")
   
-
-
